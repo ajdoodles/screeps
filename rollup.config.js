fb4f2436b2d25d976c44591f6a2a5bc00f1b42ed
Alex Duda <alexjduda@gmail.com>
2021-08-13 19:13:33 -0400

    Activate rollup's screeps plugin to upload minified code.
    This was a frustrating commit, it took too long to confirm that modules 
    main was dynamically requiring ('require(role)') were actually being 
    rolled up. I can't be sure if Atom's file tree was being updated or if 
    dist was actually being cleared by rollup's clear plugin.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,31 @@
+import clear from 'rollup-plugin-clear';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import screeps from 'rollup-plugin-screeps';
+
+let cfg;
+const dest = process.env.DEST;
+if (!dest) {
+  console.log('No destination specified for screeps code.')
+} else if ((cfg = require('./.screeps.json')[dest]) == null){
+  throw new Error('Invalid upload destination');
+}
 
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/main.min.js',
+    file: 'dist/main.js',
+    name: 'main',
     format: 'cjs',
-    sourcemap: 'inline'
+    sourcemap: true
   },
   plugins: [
-    resolve({ rootDir: 'src'}),
-    commonjs()
+    clear({ targets: ['dist'] }),
+    resolve({ rootDir: 'src' }),
+    commonjs({
+      dynamicRequireTargets: [
+        'src/roles/*.js'
+      ]}),
+    screeps({ config: cfg, dryRun: (cfg == null) })
   ]
 };
