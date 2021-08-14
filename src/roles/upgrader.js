26a79ff746505f99e8fda3d5c9a192e126b77f5b
Alex Duda <alexjduda@gmail.com>
2021-08-14 15:04:15 -0400

    Rewrite UpgraderRole to inherit from PioneerRole instead of Role.
    As title. This is untested since I haven't developed tools to recruit 
    roles at will just yet, but it compiles and doesn't crash so good 
    enough.

--- src/roles/upgrader.js
+++ src/roles/upgrader.js
@@ -1,37 +1,27 @@
-var Role = require('roles/role');
+var PioneerRole = require('roles/pioneer');
 var Utils = require('utils');
 
 const MAX_UPGRADERS = 3;
 
 function UpgraderRole() {
-  Role.call(this, 'Upgrader', 'upgrader', [WORK, CARRY, MOVE]);
+  PioneerRole.call(this, 'Upgrader', 'upgrader');
 };
-Utils.inheritFromSuperClass(UpgraderRole, Role);
+Utils.inheritFromSuperClass(UpgraderRole, PioneerRole);
 
 UpgraderRole.prototype.needsMoreRecruits = function (curCount) {
   return curCount < MAX_UPGRADERS;
 };
 
-/** @param {Creep} screep **/
-UpgraderRole.prototype.run = function(screep) {
+UpgraderRole.prototype._getNextTargetId = function (screep) {
+  return screep.room.controller.id;
+};
 
-  if(screep.memory.upgrading && screep.store[RESOURCE_ENERGY] == 0) {
-    screep.memory.upgrading = false;
-    screep.say('🔄 harvest');
-  }
-  if(!screep.memory.upgrading && screep.store.getFreeCapacity() == 0) {
-    screep.memory.upgrading = true;
-    screep.say('⚡ upgrade');
-  }
+UpgraderRole._doWork = function (screep, target) {
+  return screep.upgradeController(screep.room.controller);
+};
 
-  if(screep.memory.upgrading) {
-    if(screep.upgradeController(screep.room.controller) == ERR_NOT_IN_RANGE) {
-      screep.moveTo(screep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-    }
-  }
-  else {
-    this.fetchEnergy(screep, screep.room.controller);
-  }
+UpgraderRole._isWorkDone = function (screep, target) {
+  return false;
 };
 
 module.exports = new UpgraderRole();
