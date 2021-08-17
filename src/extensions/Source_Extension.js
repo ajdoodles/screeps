2990572720a9e5f8ade93eab86d7d8f4eedb4a63
Alex Duda <alexjduda@gmail.com>
2021-08-16 20:20:28 -0400

    Extend Source prototype with method for finding optimal buffer position.
    Uses the walkable surroundings method from the last commit to find an 
    optimal, adjacent tile to a source for a buffer container. For now the 
    optimal position is the one with the most adjacent walkable space.
    
    TODO: Consider distance to important structures in the event of a tie.

--- src/extensions/Source_Extension.js
+++ src/extensions/Source_Extension.js
@@ -14,6 +14,35 @@ module.exports = (function(){
 
   Object.defineProperty(
     Source.prototype,
+    'bufferPos',
+    {
+      get: function() {
+        if (!this._bufferPos) {
+          if (!this.memory.bufferPos) {
+            const {x, y} = this.pos;
+            var surroundings = this.room.getWalkableSurroundings(x, y);
+            console.log('found surrounds ' + Object.entries(surroundings));
+            var mostFreeSpaces = 0
+            var mostFreePos = surroundings.reduce((moreFreePos, nextPos) => {
+              let nextSurroundings = this.room.getWalkableSurroundings(nextPos.x, nextPos.y);
+              let nextFreeSpaces = nextSurroundings.length;
+              if (mostFreeSpaces >= nextFreeSpaces) {
+                return moreFreePos;
+              }
+              mostFreeSpaces = nextFreeSpaces;
+              return nextPos;
+            });
+            this.memory.bufferPos = mostFreePos;
+          }
+          this._bufferPos = this.memory.bufferPos;
+        }
+        return this._bufferPos;
+      }
+    }
+  );
+
+  Object.defineProperty(
+    Source.prototype,
     'buffer',
     {
       get: function () {
