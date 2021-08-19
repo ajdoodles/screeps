30a4ee12d90b066c48bc9a415c08d861971d0dc8
Alex Duda <alexjduda@gmail.com>
2021-08-18 20:50:39 -0400

    Tweak MineSurveyor to build buffers one at a time, in order by distance
    Prior to this commit MineSurveyor would plant buffers for all sources in 
    a room simultaneously. A call to `survey(roomName)` will now plant a 
    construction site on the nearest source without a buffer only. If the 
    buffer is still being built, it simply tries again in the same spot and 
    fails.
    
    TODO: I swear this game is fucking with me. The RoomPosition objects I'm 
    creating are inheriting from Object (as in pos.prototype === 
    Object.prototype). I'm manually fixing the prototype before calling 
    `PathFinder.search()`. I need to test this in a clean branch to see if 
    my code is messing this up somewhere or if it's a true bug in screeps 
    server code.

--- src/surveyors/mines.js
+++ src/surveyors/mines.js
@@ -8,13 +8,15 @@ Utils.inheritFromSuperClass(MineSurveyor, Surveyor);
 
 MineSurveyor.prototype.survey = function (roomName) {
   const room = Game.rooms[roomName];
-  room.sources.forEach((source) => {
-    let buffer = source.buffer;
-    if (!buffer) {
-      let bufferPos = source.bufferPos;
-      room.createConstructionSite(bufferPos.x, bufferPos.y, STRUCTURE_CONTAINER);
-    }
-  });
+
+  var sourcesWithoutBuffers = room.sources.filter((source) => !source.buffer);
+  if (sourcesWithoutBuffers.length > 0) {
+    var bufferPosList = sourcesWithoutBuffers.map((source) => source.bufferPos);
+    bufferPosList.forEach((pos) => Object.setPrototypeOf(pos, Object.create(RoomPosition.prototype)));
+    var results = PathFinder.search(room.mainSpawn.pos, bufferPosList);
+    var newBufferPos = results.path[results.path.length - 1];
+    room.createConstructionSite(newBufferPos.x, newBufferPos.y, STRUCTURE_CONTAINER);
+  }
 };
 
 module.exports = new MineSurveyor();
