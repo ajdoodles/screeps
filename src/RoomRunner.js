a74af2e6002158f41763d7b92edaea8662a4e561
Alex Duda <alexjduda@gmail.com>
2021-08-21 19:04:30 -0400

    Add missing semicolon.
    TODO: lint...

--- src/RoomRunner.js
+++ src/RoomRunner.js
@@ -14,7 +14,7 @@ module.exports = (function(){
         sourcePos = source.bufferPos;
       }
       pioneerAllowance += room.getWalkableSurroundings(sourcePos.x, sourcePos.y).length;
-    })
+    });
     return pioneerAllowance;
   };
 
