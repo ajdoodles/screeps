07f34f56f72270a8529ad08505b5ef9b55708caa
Alex Duda <alexjduda@gmail.com>
2021-08-21 18:15:03 -0400

    Set default room logic to saturate a room with harvesters over time.
    Default room logic now calculates the number of harvesters requires to 
    saturate every energy source in the room and attempts to recruit to fill 
    the gap every nine ticks. Energy saturation capacity is the total sum of 
    the free space surrounding each source in the room (or the source's 
    buffer if it exists).

--- src/RoomRunner.js
+++ src/RoomRunner.js
@@ -4,10 +4,29 @@ module.exports = (function(){
   var mRecruiter = require('recruiter');
   var mRoomHandlers = Object.create(null);
 
-  var runDefault = function(room) {
-    mRecruiter.recruit(room, [Roles.HARVESTER]);
+  // How many harvesters do we need to saturate all of the energy sources in the
+  // room.
+  var _calculateMaxHarvesters = function(room) {
+    var harvesterAllowance = 0;
+    room.sources.forEach((source) => {
+      let sourcePos = source.pos;
+      if (source.buffer) {
+        sourcePos = source.bufferPos;
+      }
+      harvesterAllowance += room.getWalkableSurroundings(sourcePos.x, sourcePos.y).length;
+    })
+    return harvesterAllowance;
   };
 
+  var runDefault = function(room) {
+    if (Game.time % 9 === 0) {
+      var curHarvesters = mRecruiter.getRoleCount(room, Roles.HARVESTER);
+      var maxHarvesters = _calculateMaxHarvesters(room);
+      mRecruiter.recruit(room, [Roles.HARVESTER]);
+    }
+  };
+
+  mRoomHandlers[0] = function(room) {
   var run = function(roomName) {
     var room = Game.rooms[roomName];
     var controllerLevel = room.controller.level;
