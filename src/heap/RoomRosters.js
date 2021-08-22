51971a3f8c243013af1cbae8e3e58efd299ce85b
Alex Duda <alexjduda@gmail.com>
2021-08-21 23:08:09 -0400

    Add RoomRosters class which will manage a cache of rosters on the heap.
    This is the first structure being managed in heap memory. Importing the 
    module initializes a cache on the `global` object. The cache is an 
    object with no prototype, whose properties are room names. Each room 
    name points to a roster for the room. Attempting to access a room's 
    roster will cause the roster to be created if it doesn't exist. The 
    roster is a map of sets, from roles to a set of creep names.

---
+++ src/heap/RoomRosters.js
@@ -0,0 +1,67 @@
+module.exports = (function(){
+
+  global.RoomRosters = global.RoomRosters || Object.create(null);
+
+  var _addCreepNameToRoomRoster = function(roomRoster, creepName) {
+    if (!roomRoster.has(Memory.creeps[creepName].role)) {
+      roomRoster.set(Memory.creeps[creepName].role, new Set());
+    }
+    roomRoster.get(Memory.creeps[creepName].role).add(creepName);
+  };
+
+  var _removeCreepNameFromRoomRoster = function(roomRoster, creepName) {
+    var roleRoster = roomRoster.get(Memory.creeps[creepName].role);
+    roleRoster.delete(creepName);
+    if (roleRoster.size === 0) {
+      roomRoster.delete(Memory.creeps[creepName].role);
+    }
+  };
+
+  var _getRosterForRoom = function(room) {
+    if (!global.RoomRosters[room.name]) {
+      let roomRoster = new Map();
+
+      room.find(FIND_MY_CREEPS).forEach((creep) => {
+        _addCreepNameToRoomRoster(roomRoster, creep.name);
+      });
+
+      global.RoomRosters[room.name] = roomRoster;
+    }
+    return global.RoomRosters[room.name];
+  };
+
+  var getRoleCount = function(room, role) {
+    _getRosterForRoom(room).get(role).size;
+  };
+
+  var addCreepNameToRoster = function(room, creepName) {
+    var roomRoster = _getRosterForRoom(room);
+    _addCreepNameToRoomRoster(roomRoster, creepName);
+  };
+
+  var removeCreepNameFromRoster = function(room, creepName) {
+    var roomRoster = _getRosterForRoom(room);
+    _removeCreepNameFromRoomRoster(roomRoster, creepName);
+  };
+
+  var logRoomRoster = function(room) {
+    var roomRoster = _getRosterForRoom(room);
+    let log;
+    for (const [role, roster] of roomRoster.entries()) {
+      log = role + ': ';
+      for (const name of roster.values()) {
+        log += name + ', ';
+      }
+      console.log(log);
+    }
+  };
+
+  var mPublic = {
+    getRoleCount: getRoleCount,
+    addCreepNameToRoster: addCreepNameToRoster,
+    removeCreepNameFromRoster: removeCreepNameFromRoster,
+    logRoomRoster: logRoomRoster,
+  };
+
+  return mPublic;
+})();
