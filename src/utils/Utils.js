f8515f5cbd45ce927ea5b712fcbc618dbca1a673
Alex Duda <alexjduda@gmail.com>
2021-08-24 14:05:48 -0400

    Add a Utils file with function to get energy cost of an array of body parts.

---
+++ src/utils/Utils.js
@@ -0,0 +1,11 @@
+module.exports = (function(){
+  var getBodyCost = function(body) {
+    return body.reduce((totalCost, bodyPart) => totalCost + BODYPART_COST[bodyPart], 0);
+  };
+
+  var mPublic = {
+    getBodyCost: getBodyCost,
+  };
+
+  return mPublic;
+})();
