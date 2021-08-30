78ceb783cfa225e62df521527fd774699d10ea95
Alex Duda <alexjduda@gmail.com>
2021-08-30 16:58:29 -0400

    Switch existing surveyors to new ConstructionQueues model.
    Instead of directly placing construction sites, Surveyors will now 
    provide a list of references to projects that still need construction in 
    each room. These will be cached on init, and every once in a while 
    Foreman (to be built) will pull the next set of projects from the 
    queues, and have Surveyors unpack them into construction sites.

--- src/surveyors/mines.js
+++ src/surveyors/mines.js
@@ -1,22 +1,39 @@
 var Surveyor = require('surveyors/surveyor');
 var Utils = require('utils');
 
+var mConstructionQueues = require('../heap/ConstructionQueues');
+
 function MineSurveyor() {
   Surveyor.call(this);
 };
 Utils.inheritFromSuperClass(MineSurveyor, Surveyor);
 
-MineSurveyor.prototype.survey = function (roomName) {
-  const room = Game.rooms[roomName];
+MineSurveyor.prototype.survey = function (room) {
+  if (!mConstructionQueues.isEmpty(BuildTypes.MINES)) {
+    return;
+  }
 
   var sourcesWithoutBuffers = room.sources.filter((source) => !source.buffer);
-  if (sourcesWithoutBuffers.length > 0) {
-    var bufferPosList = sourcesWithoutBuffers.map((source) => source.bufferPos);
-    bufferPosList.forEach((pos) => Object.setPrototypeOf(pos, Object.create(RoomPosition.prototype)));
-    var results = PathFinder.search(room.mainSpawn.pos, bufferPosList);
-    var newBufferPos = results.path[results.path.length - 1];
-    room.createConstructionSite(newBufferPos.x, newBufferPos.y, STRUCTURE_CONTAINER);
+
+  if (sourcesWithoutBuffers.length === 0) {
+    return;
   }
+
+  var sourceDistances = Object.create(null);
+  sourcesWithoutBuffers.forEach((source) => {
+    var results = PathFinder.search(
+      room.mainSpawn.pos,
+      {pos: source.pos, range: 1},
+      {swampCost: 1});
+    sourceDistances[source.id] = results.path.length;
+  }).sort((firstSource, secondSource) => {
+    sourceDistances[firstSource.id] - sourceDistances[secondSource.id];
+  }).forEach((source) => mConstructionQueues.queue(BuildTypes.MINES, source));
+};
+
+MineSurveyor.prototype.planConstruction = function(source) {
+  var {x, y} = source.bufferPos;
+  room.createConstructionSite(x, y, STRUCTURE_CONTAINER);
 };
 
 module.exports = new MineSurveyor();
--- src/surveyors/roads.js
+++ src/surveyors/roads.js
@@ -1,13 +1,54 @@
+var BuildTypes = require('../constants/BuildTypes');
 var Surveyor = require('surveyors/surveyor');
 var Utils = require('utils');
+
+var mConstructionQueues = requires('../heap/ConstructionQueues');
 
 function RoadSurveyor() {
   Surveyor.call(this);
 };
 Utils.inheritFromSuperClass(RoadSurveyor, Surveyor);
 
-RoadSurveyor.prototype._buildRoad = function (x, y, roomName) {
-  var room = Game.rooms[roomName];
+RoadSurveyor.prototype._queueSurroundProject = function(center) {
+  mConstructionQueues.queue(BuildTypes.ROADS, [center]);
+};
+
+RoadSurveyor.prototype._queuePathProject = function(from, to) {
+  mConstructionQueues.queue(BuildTypes.ROADS, [from, to]);
+};
+
+RoadSurveyor.prototype.survey = function(room) {
+  if (!mConstructionQueues.isEmpty()) {
+    return;
+  }
+
+  var sourceDistances = Object.create(null);
+  var sources = room.sources.forEach((source) => {
+    var results = PathFinder.search(
+      room.mainSpawn.pos,
+      {pos: source.pos, range: 1},
+      {swampCost: 1});
+    sourceDistances[source.id] = results.path.length;
+  }).sort((firstSource, secondSource) => {
+    sourceDistances[firstSource.id] - sourceDistances[secondSource.id];
+  });
+
+  var controllerSource = room.controller.pos.findClosestByPath(FIND_SOURCES);
+
+  this._queuePathProject(room.mainSpawn, sources.shift());
+  this._queuePathProject(room.mainSpawn, controllerSource);
+  this._queuePathProject(room.controller, controllerSource);
+  sources.forEach((source) => {
+    if (source.id !== controllerSource.id) {
+      this._queuePathProject(room.mainSpawn, source);
+    }
+  });
+
+  this._queueSurroundProject(room.mainSpawn);
+  room.sources.forEach((source) => this._queueSurroundProject(source));
+};
+
+RoadSurveyor.prototype._buildRoad = function (room, x, y) {
   var stopBuild = false;
 
   if (room.getTerrain().get(x, y) === TERRAIN_MASK_WALL) {
@@ -30,8 +71,7 @@ RoadSurveyor.prototype._buildRoad = function (x, y, roomName) {
 };
 
 RoadSurveyor.prototype._surroundWithRoad = function(object) {
-  var x = object.pos.x;
-  var y = object.pos.y;
+  var {x, y} = object.pos;
   [x - 1, x, x + 1].forEach(
     (i) => [y - 1, y, y + 1].forEach(
       (j) => this._buildRoad(i, j, object.room.name)));
@@ -43,23 +83,12 @@ RoadSurveyor.prototype._connectWithRoad = function(firstObj, secondObj) {
   path.forEach((step) => this._buildRoad(step.x, step.y, room.name));
 };
 
-RoadSurveyor.prototype.survey = function(roomName) {
-  var room = Game.rooms[roomName];
-  var spawns = room.find(FIND_MY_SPAWNS);
-  var sources = room.find(FIND_SOURCES);
-
-  spawns.forEach((spawn) => {
-    this._surroundWithRoad(spawn);
-    sources.forEach((source) => this._connectWithRoad(spawn, source));
-  });
-
-  var controllerSource = room.controller.pos.findClosestByPath(FIND_SOURCES);
-  this._surroundWithRoad(room.controller);
-  this._connectWithRoad(room.controller, controllerSource);
-
-  sources.forEach((source) => {
-    this._surroundWithRoad(source);
-  });
+RoadSurveyor.prototype.planConstruction = function(project) {
+  if (project.length === 1) {
+    _surroundWithRoad(project[0]);
+  } if (project.length === 2) {
+    _connectWithRoad(project[0], project[1]);
+  }
 };
 
 module.exports = new RoadSurveyor();
