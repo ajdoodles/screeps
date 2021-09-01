a1d639cb31a9c58f359eaa453a0468d9b5ac3613
Alex Duda <alexjduda@gmail.com>
2021-08-31 22:10:51 -0400

    Change RoadSurveyor.planConstruction to generate blueprints for planning
    RoadSurveyor.planConstruction used to be directly responsible for 
    placing road construction sites. Now, as with MineSurveyor, RoadSurveyor 
    generates a list of RoomPositions and adds them to a blueprint object. 
    This blueprint will be unpacked later and used to created construction 
    sites.

--- src/surveyors/roads.js
+++ src/surveyors/roads.js
@@ -9,11 +9,11 @@ function RoadSurveyor() {
 Classes.inheritFromSuperClass(RoadSurveyor, Surveyor);
 
 RoadSurveyor.prototype._queueSurroundProject = function(room, center) {
-  ConstructionQueues.enqueue(room, BuildTypes.ROADS, [center]);
+  ConstructionQueues.enqueue(room, BuildTypes.ROADS, [center.id]);
 };
 
 RoadSurveyor.prototype._queuePathProject = function(room, from, to) {
-  ConstructionQueues.enqueue(room, BuildTypes.ROADS, [from, to]);
+  ConstructionQueues.enqueue(room, BuildTypes.ROADS, [from.id, to.id]);
 };
 
 RoadSurveyor.prototype.survey = function(room) {
@@ -49,46 +49,57 @@ RoadSurveyor.prototype.survey = function(room) {
   room.sources.forEach((source) => this._queueSurroundProject(room, source));
 };
 
-RoadSurveyor.prototype._buildRoad = function (room, x, y) {
+RoadSurveyor.prototype._shouldBuild = function (room, x, y) {
   var stopBuild = false;
 
-  if (room.getTerrain().get(x, y) === TERRAIN_MASK_WALL) {
-    return;
-  }
+  stopBuild |= room.getTerrain().get(x, y) === TERRAIN_MASK_WALL;
 
   var sites = room.lookForAt(LOOK_CONSTRUCTION_SITES, x, y);
-  stopBuild = sites.reduce((stopBuild, site) => (stopBuild || site.type !== STRUCTURE_RAMPART || site.type !== STRUCTURE_CONTAINER), false);
-  if (stopBuild) {
-    return;
-  }
+  stopBuild |= sites.reduce((stopBuild, site) => (stopBuild || site.type !== STRUCTURE_RAMPART || site.type !== STRUCTURE_CONTAINER), false);
 
   var structures = room.lookForAt(LOOK_STRUCTURES, x, y);
-  stopBuild = structures.reduce((stopBuild, struct) => (stopBuild || struct.type !== STRUCTURE_RAMPART || struct.type !== STRUCTURE_CONTAINER), false);
-  if (stopBuild) {
-    return;
-  }
+  stopBuild |= structures.reduce((stopBuild, struct) => (stopBuild || struct.type !== STRUCTURE_RAMPART || struct.type !== STRUCTURE_CONTAINER), false);
 
-  room.createConstructionSite(x, y, STRUCTURE_ROAD);
+  return stopBuild;
 };
 
-RoadSurveyor.prototype._surroundWithRoad = function(room, object) {
+RoadSurveyor.prototype._generateWalkwayPlans = function(room, id) {
+  var object = Game.getObjectById(id);
   var {x, y} = object.pos;
+  var positions = [];
   [x - 1, x, x + 1].forEach(
     (i) => [y - 1, y, y + 1].forEach(
-      (j) => this._buildRoad(i, j, room.name)));
+      (j) => {
+        if (!(i === x && j === y) && this._shouldBuild(room, i, j)) {
+          positions.push(room.getPositionAt(i, j));
+        }
+      }
+    )
+  );
+  return positions;
 };
 
-RoadSurveyor.prototype._connectWithRoad = function(room, firstObj, secondObj) {
-  var path = room.findPath(firstObj.pos, secondObj.pos, {ignoreCreeps: true, ignoreRoads: true, swampCost: 1});
-  path.forEach((step) => this._buildRoad(step.x, step.y, room.name));
+RoadSurveyor.prototype._generateRoadPlans = function(room, firstId, secondId) {
+  var firstObj = Game.getObjectById(firstId);
+  var secondObj = Game.getObjectById(secondId);
+  var path = room.findPath(
+    firstObj.pos,
+    secondObj.pos,
+    {ignoreCreeps: true, swampCost: 1});
+  return path;
 };
 
 RoadSurveyor.prototype.planConstruction = function(room, project) {
+  var positions;
   if (project.length === 1) {
-    _surroundWithRoad(room, project[0]);
+    positions = this._generateWalkwayPlans(room, project[0]);
   } if (project.length === 2) {
-    _connectWithRoad(room, project[0], project[1]);
+    positions = this._generateRoadPlans(room, project[0], project[1]);
   }
+
+  var roadPlans = Object.create(null);
+  roadPlans[STRUCTURE_ROAD] = positions;
+  ConstructionQueues.setPlannedConstruction(room, BuildTypes.ROADS, roadPlans);
 };
 
 module.exports = new RoadSurveyor();
