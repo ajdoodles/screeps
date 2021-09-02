2dbb8a0139ab7bb3d982cae564293dec11fc3ff2
Alex Duda <alexjduda@gmail.com>
2021-09-02 00:20:15 -0400

    Install eslint-plugin-screeps to supress screeps specific warnings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-      "eslint:recommended",
-      "prettier"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    "screeps/screeps": true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+  plugins: ["screeps"],
 };
