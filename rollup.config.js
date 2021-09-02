72e27021fc361349f47c07aa14f2c53209313b73
Alex Duda <alexjduda@gmail.com>
2021-09-02 01:14:30 -0400

    Run config files through prettier for good measure.

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,34 @@
-import clear from 'rollup-plugin-clear';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import screeps from 'rollup-plugin-screeps';
+import clear from "rollup-plugin-clear";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import screeps from "rollup-plugin-screeps";
 
 let cfg;
 const dest = process.env.DEST;
 if (!dest) {
-  console.log('No destination specified for screeps code.')
-} else if ((cfg = require('./.screeps.json')[dest]) == null){
-  throw new Error('Invalid upload destination');
+  console.log("No destination specified for screeps code.");
+} else if ((cfg = require("./.screeps.json")[dest]) == null) {
+  throw new Error("Invalid upload destination");
 }
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
-    file: 'dist/main.js',
-    name: 'main',
-    format: 'cjs',
-    sourcemap: true
+    file: "dist/main.js",
+    name: "main",
+    format: "cjs",
+    sourcemap: true,
   },
   plugins: [
-    clear({ targets: ['dist'] }),
-    resolve({ rootDir: 'src' }),
+    clear({ targets: ["dist"] }),
+    resolve({ rootDir: "src" }),
     commonjs({
       dynamicRequireTargets: [
-        'src/roles/*.js',
-        '!src/roles/pioneer.js',
-        '!src/roles/role.js'
-      ]
+        "src/roles/*.js",
+        "!src/roles/pioneer.js",
+        "!src/roles/role.js",
+      ],
     }),
-    screeps({ config: cfg, dryRun: (cfg == null) })
-  ]
+    screeps({ config: cfg, dryRun: cfg == null }),
+  ],
 };
