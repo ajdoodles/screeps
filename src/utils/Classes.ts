ed506b625c135ae97e1d63ebdcd5cf9f6c1523e6
Alex Duda <alexjduda@gmail.com>
2021-09-09 17:36:14 -0400

    Convert Classes.js to Classes.ts.

---
+++ src/utils/Classes.ts
@@ -0,0 +1,11 @@
+export function inheritFromSuperClass(
+  subClass: Function,
+  superClass: Function
+) {
+  subClass.prototype = Object.create(superClass.prototype);
+  Object.defineProperty(subClass.prototype, "constructor", {
+    value: subClass,
+    enumerable: false, // so that it does not appear in 'for in' loop
+    writable: true,
+  });
+}
