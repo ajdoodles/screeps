2eecbbdc369bd81906cdf7d6546937bbb9af164b
Alex Duda <alexjduda@gmail.com>
2021-09-09 19:49:30 -0400

    Convert ConstructionQueues.js to ConstructionQueus.ts.

---
+++ src/heap/ConstructionQueues.ts
@@ -0,0 +1,50 @@
+import Heap from "./RoomsHeap";
+
+class ConstructionQueue extends Array<Object> {
+  planned: Record<StructureConstant, RoomPosition[]> = Object.create(null);
+}
+
+function getQueues(room: Room): Record<string, ConstructionQueue> {
+  room.heap.constructionQueues =
+    room.heap.constructionQueues || Object.create(null);
+  return room.heap.constructionQueues;
+}
+
+function getQueue(room: Room, buildType: string): ConstructionQueue {
+  var queues = getQueues(room);
+  queues[buildType] = queues[buildType] || new ConstructionQueue();
+  return queues[buildType];
+}
+
+export function isEmpty(room: Room, buildType: string) {
+  return getQueue(room, buildType).length === 0;
+}
+
+export function peek(room: Room, buildType: string) {
+  return isEmpty(room, buildType) ? undefined : getQueue(room, buildType)[0];
+}
+
+export function enqueue(room: Room, buildType: string, project: Object) {
+  getQueue(room, buildType).push(project);
+}
+
+export function dequeue(room: Room, buildType: string) {
+  return getQueue(room, buildType).shift();
+}
+
+export function hasPlannedConstruction(room: Room, buildType: string) {
+  var plannedWork = getQueue(room, buildType).planned;
+  return Object.getOwnPropertyNames(plannedWork).length !== 0;
+}
+
+export function getPlannedConstruction(room: Room, buildType: string) {
+  return getQueue(room, buildType).planned;
+}
+
+export function setPlannedConstruction(
+  room: Room,
+  buildType: string,
+  plans: Record<StructureConstant, RoomPosition[]>
+) {
+  getQueue(room, buildType).planned = plans;
+}
